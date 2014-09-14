5798a56a596dbbc9dbbce5f151f5c651f3fd9492
AndiH <a.herten+github@gmail.com>
2014-09-14 18:54:35 +0200

    Rephrased footer text slightly to be more general.

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 	
 	<footer>
-		<p>powered by <a href="http://personalapi.org/">Personal API</a>, bachelor thesis project of <a href="http://stefangrund.de/">Stefan Grund</a></p>
+		<p>Powered by <a href="http://personalapi.org/">Personal API</a>, based on the bachelor thesis project of <a href="http://stefangrund.de/">Stefan Grund</a>.</p>
 	</footer>
 
 	</div><!-- /content -->	
